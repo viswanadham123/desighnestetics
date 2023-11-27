9757414b806c1af6dcae8364668a3eaf35b61273
viswanadham123 <viswanadham.andala@gmail.com>
2023-11-27 22:21:22 +0530

    zoom adijust

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 import MainPage from "./components/MainPage"
 
 function App() {
+  useEffect(() => {
+    document.body.style.zoom = "67%";
+      }, []);
   return (
     <MainPage/>
   );
--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -366,16 +366,16 @@ const MainPage = () => {
                 </div>
               </div>
               <div className="maps-div">
-                <iframe
-                  title="Google Map"
-                  srcDoc="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31103.159728471357!2d77.58904125000001!3d12.9785693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1701084738589!5m2!1sen!2sin"
+              <iframe
+                  className="maps-div"
+                  src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d124430.18465116947!2d77.52733163906251!3d12.943463001689041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1701069592483!5m2!1sen!2sin"
                   width="600"
                   height="450"
                   style={{ border: 0 }}
-                  allowFullScreen
+                  allowFullScreen=""
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
-                />
+                ></iframe>
               </div>
             </div>
           ) : (
